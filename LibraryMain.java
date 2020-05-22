df201ed8224c463befe076a9b986f92538741a26
Keng Moua <keng@Kengs-MacBook-Pro.local>
2020-05-21 23:57:43 -0400

    final push

--- LibraryMain.java
+++ LibraryMain.java
@@ -3,6 +3,10 @@ public class LibraryMain {
 
 	public static void main(String[] args) {
 		Author author1 = new Author("Stephen", "King");
+		System.out.println(author1.getAuthor());
+		
+		LibraryItem lib1 = LibraryItemFactory.createLibraryItem(LibraryItemFactory.ItemType.NEWSPAPER);
+		lib1.setAuthor(author1);
 	}
 
 }
